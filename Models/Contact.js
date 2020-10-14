bdcf0e0768603dae5bcda1fb9f398148e3f354cd
IA Masum <38923620+IA-Masum@users.noreply.github.com>
2020-10-15 00:52:33 +0600

    Contact Schema set

---
+++ Models/Contact.js
@@ -0,0 +1,24 @@
+const {Schema} = require('mongoose');
+
+
+const contactSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength:2,
+        maxlength:14
+    },
+    phone: {
+        type: String,
+        required: true,
+        trim: true,
+        minLength: 9,
+        maxlength: 15
+    },
+    email: {
+        type: String,
+        required: true,
+        trim: true
+    }
+});
--- app.js
+++ app.js
@@ -3,35 +3,15 @@ const mongoose = require('mongoose');
 
 const app = express();
 
-app.set('view engine', 'ejs');
+// app.set('view engine', 'ejs');
 
-let Schema = mongoose.Schema;
-
-let testSchema = new Schema({
-    name: String
-});
-
-
-let Test = mongoose.model('Test', testSchema);
-
-
-app.get('/', (req, res)=>{
-
-    let test = new Test({
-        name: "IA Masum"
-    });
-
-    test.save()
-        .then(t => res.send(t))
-        .catch(err => console.log(err));
-})
 
 
 const PORT = process.env.PORT || 3030;
 
 mongoose.connect(
         `mongodb+srv://Imran_ali:kBDDbrdXC3vT55k@cluster0.wkjey.mongodb.net/test?retryWrites=true";`,
-        {useNewUrlParser: true}
+    {useNewUrlParser: true, useUnifiedTopology: true}
      )
     .then(() => {
         app.listen(PORT, () => console.log(`Server is Running on PORT: ${PORT} .`));
